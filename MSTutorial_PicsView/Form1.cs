64848b4661bf107411cc9cbef48990304eebfac5
HAJIME Fukuna <f97one@gmail.com>
2012-09-02 20:24:19 +0900

    フォームのText、Sizeの各プロパティを修正。

--- MSTutorial_PicsView/Form1.cs
+++ MSTutorial_PicsView/Form1.cs
@@ -15,5 +15,10 @@ namespace MSTutorial_PicsView
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
